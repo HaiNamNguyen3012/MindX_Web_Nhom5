71fbeb9c632984127b78e7eba53ff42958a042c5
Trang1507 <thuytrang150715@gmail.com>
2021-11-06 16:52:59 +0700

    update

--- GROUP WORK 1ST PAGE/javascript/renderfile.js
+++ GROUP WORK 1ST PAGE/javascript/renderfile.js
@@ -16,21 +16,21 @@ let popular_brand_photo = [
 {
     photo: "PHOTO/SHO4.jpeg", 
     url:'#',
-},
+}
 ];
 
 function render_popular_brand_photo(){
-    popular_brand_photo.innerHTML += popular_brand_photo.innerHTML + "
-    <div class="photo_1">
+    popular_brand_photo.innerHTML = popular_brand_photo.innerHTML + `
+    <div class = "photo_1">
         
-    </div>
-    ";
+
+    </div>`;
     for (let i =0;i<popular_brand_photo.length;i=i+1) {
-        document.getElementsByClassName('photo_1') += '
-        <a href = "'+ popular_brand_photo[i].photo + ' ">
-        <img src= " ' + popular_brand_photo[i].url + ' ">
+        document.getElementsByClassName("photo_1") += `
+        <a href = "`+ popular_brand_photo[i].photo + ` ">
+        <img src= "` + popular_brand_photo[i].url +  `">
         </a> 
-        ';
+        `;
     }
 }
 render_popular_brand_photo();
