9f3ddff8ff6ea88c761ab67163ab95cdac6cb86a
Trang1507 <thuytrang150715@gmail.com>
2021-11-06 17:23:02 +0700

    update

--- GROUP WORK 1ST PAGE/javascript/renderfile.js
+++ GROUP WORK 1ST PAGE/javascript/renderfile.js
@@ -1,6 +1,6 @@
 let popular_brand_photo = document.getElementsByClassName("popular_brand_photo");
 
-let popular_brand_photo = [ 
+let popular_brand_photo_data = [ 
 {
     photo: "PHOTO/SHO1.jpeg",
     url:'#',
@@ -20,15 +20,15 @@ let popular_brand_photo = [
 ];
 
 function render_popular_brand_photo(){
-    popular_brand_photo.innerHTML = popular_brand_photo.innerHTML + `
+    popular_brand_photo.innerHTML +=  `
     <div class = "photo_1">
         
 
     </div>`;
-    for (let i =0;i<popular_brand_photo.length;i=i+1) {
+    for (let i = 0;i<popular_brand_photo_data.length;i=i+1) {
         document.getElementsByClassName("photo_1") += `
-        <a href = "`+ popular_brand_photo[i].photo + ` ">
-        <img src= "` + popular_brand_photo[i].url +  `">
+        <a href = "`+ popular_brand_photo_data[i].url +`">
+        <img src= "`+ popular_brand_photo_data[i].photo +`">
         </a> 
         `;
     }
