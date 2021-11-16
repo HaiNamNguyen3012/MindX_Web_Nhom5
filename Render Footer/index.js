c4d8c975d8bb3e18ecf879e97da20cc0d8a25d4d
haucaibat110 <88539718+haucaibat110@users.noreply.github.com>
2021-11-16 14:27:58 +0700

    Create index.js

---
+++ Render Footer/index.js
@@ -0,0 +1,127 @@
+
+let page = document.getElementById('main');
+
+
+
+
+function render_footer(){
+    
+
+            page.innerHTML +=
+            `
+            <div class="app">
+                 <footer class="footer"> 
+                 <div class="grid-footer">
+                         <div class="grid__row"> 
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Air Jordan</h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/retro-jordans/air-jordan-1" class="footer_item-link">Air Jordan 1</a>
+                                                        <a href="https://stockx.com/retro-jordans/golf" class="footer_item-link">Jordan Golf Shoes</a>
+                                                        <a href="https://stockx.com/retro-jordans/air-jordan-3" class="footer_item-link">Air Jordan 3</a>
+                                                        <a href="https://stockx.com/retro-jordans/air-jordan-11" class="footer_item-link">Air Jordan 11 </a>
+                                                        <a href="https://stockx.com/retro-jordans/air-jordan-11/low" class="footer_item-link">Air Jordan 11 Low</a>
+                                                        <a href="https://stockx.com/retro-jordans/air-jordan-1/mid" class="footer_item-link">Jordan 1 Mid</a>
+                                                </li>
+                                        </ul>
+                                </div>
+
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Yeezy</h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/adidas/yeezy/350" class="footer_item-link">Yeezy Boost 350</a>
+                                                        <a+ href="https://stockx.com/adidas/yeezy/350/v2" class="footer_item-link">Yeezy Boost 350 V2</a>
+                                                        <a href="https://stockx.com/adidas/yeezy/700" class="footer_item-link">Yeezy Boost 700</a>
+                                                        <a href="https://stockx.com/adidas/yeezy/500" class="footer_item-link">Yeezy 500 </a>
+                                                        <a href="https://stockx.com/other-sneakers/yeezy/slides" class="footer_item-link">Yeezy slide</a>
+                                                        <a href="https://stockx.com/adidas/yeezy/foam-runner" class="footer_item-link">Yeezy Foam RNNR</a>
+                                                </li>
+                                        </ul>
+                                </div>
+
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Recent Updates </h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/news/the-drop-list-10-04-21/" class="footer_item-link">The Drop List</a>
+                                                        <a href="https://stockx.com/news/a-preview-of-the-new-nintendo-switch-oled/" class="footer_item-link">Nlntendo Switch OLED</a>
+                                                        <a href="https://stockx.com/news/the-10-most-expensive-sneakers-in-stockx-history/" class="footer_item-link">Most Expensive Sneakers</a>
+                                                        <a href="https://stockx.com/electronics/xbox/series-x" class="footer_item-link">Xbox Series X </a>
+                                                        <a href="https://stockx.com/electronics/playstation/ps5" class="footer_item-link">PS5</a>
+                                                        <a href="https://stockx.com/news/yeezy-x-gap-hoodie-pick-of-the-week/" class="footer_item-link">Yeezy GAP Hoodies</a>
+                                                </li>
+                                        </ul>
+                                </div>
+
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Popular releases</h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/nike-dunk-high-midnight-navy" class="footer_item-link">Nike Dunk High Vlllanova</a>
+                                                        <a href="https://stockx.com/air-jordan-5-retro-bluebird-w" class="footer_item-link">Jordan 5 Bluelrd</a>
+                                                        <a href="https://stockx.com/nike-ld-waffle-clot-sacai-grey-navy" class="footer_item-link">Nike LDWaffle CLOT sacal Grey</a>
+                                                        <a href="https://stockx.com/nike-air-max-1-patta-noise-aqua" class="footer_item-link">Nike Air Max 1 Patta Waves </a>
+                                                        <a href="https://stockx.com/air-jordan-11-retro-cool-grey-2021" class="footer_item-link">Jodan 11 Cool Grey</a>
+                                                        <a href="https://stockx.com/air-jordan-1-retro-high-og-bred-patent" class="footer_item-link">Jordan 1 Bred Patent</a>
+                                                </li>
+                                        </ul>
+                                </div>
+
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Nlntendo</h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/nintendo-switch-oled-hegskabaa-neon-red-neon-blue" class="footer_item-link">Nlntendo Switch OLED Red Blue</a>
+                                                        <a href="https://stockx.com/nintendo-switch-oled-hegskaaaa-white" class="footer_item-link">Nlntendo Switch OLED White</a>
+                                                        <a href="https://stockx.com/nintendo-super-nes-classic-edition-clvssnsg" class="footer_item-link">Nlntendo Super NES</a>
+                                                        <a href="https://stockx.com/nintendo-switch-lite-grey" class="footer_item-link">Nlntendo Switch Lite Grey </a>
+                                                        <a href="https://stockx.com/electronics/nintendo/switch-lite" class="footer_item-link">Nlntendo Switch Lite Grey</a>
+                                                        <a href="https://stockx.com/electronics/nintendo/amiibo" class="footer_item-link">Nlntendo Amllbo</a>
+                                                </li>
+                                        </ul>
+                                </div>
+
+                                <div class="grid__colum-2-4">
+                                        <h3 class="footer__heading"> Supreme</h3>
+                                        <ul class="footer-list">
+                                                <li class="footer_item">
+                                                        <a href="https://stockx.com/supreme/t-shirts" class="footer_item-link">T-Shirts</a>
+                                                        <a href="https://stockx.com/supreme/accessories" class="footer_item-link">Accessories</a>
+                                                        <a href="https://stockx.com/supreme/the-north-face" class="footer_item-link">Supreme The Notth Face</a>
+                                                        <a href="https://stockx.com/supreme/bottoms" class="footer_item-link">Bottoms</a>
+                                                        <a href="https://stockx.com/supreme/sweatshirts" class="footer_item-link">Hoodies</a>
+                                                        <a href="https://stockx.com/supreme/nike-collection" class="footer_item-link">Supreme Nike</a>
+                                                </li>
+                                        </ul>
+                                </div>
+                         </div>
+                </div>
+                <hr>
+
+                <div class="footer__list-icon">
+                        <ul class="list-icon">
+                                <li class="footer_icon-link"><i class="fab fa-twitter"></i></li>
+                                <li class="footer_icon-link"><i class="fab fa-facebook-f"></i></li>
+                                <li class="footer_icon-link"><i class="fab fa-instagram"></i></li>
+                                <li class="footer_icon-link"><i class="fab fa-youtube"></i></li>        
+                        </ul>
+
+                        <ul class="list-icon">
+                                <li class="footer_icon-link footer_icon-link2 "><i class="fab fa-apple"></i></li>
+                                <li class="footer_icon-link footer_icon-link2"><i class="fab fa-google"></i></li>         
+                        </ul>
+                </div>
+        </footer> 
+          
+        </div>
+            `
+        
+    }
+    
+
+
+render_footer();
+
+
